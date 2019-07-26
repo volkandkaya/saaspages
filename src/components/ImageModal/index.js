97de69ee69373b329b50e4bb54e05689669b9458
volkan <volkan.kaya@hotmail.co.uk>
2019-07-26 18:43:49 +0100

    arrow keys

--- src/components/ImageModal/index.js
+++ src/components/ImageModal/index.js
@@ -1,5 +1,5 @@
 import { Link } from 'gatsby'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Lightbox } from 'react-modal-image'
 import Icon from 'components/Icon'
 
@@ -28,15 +28,24 @@ const ImageModal = ({ss, onClose, screenshots, setSS, filterName}) => {
 
   const onKeyDown = (e) => {
     if(e.keyCode === 37) {
-      nextSS(e, index - 1)
+      if (index !== 0) {
+        nextSS(e, index - 1)
+      }
     }
     if(e.keyCode === 39) {
-      nextSS(e, index + 1)
+      if (index !== screenshots.length - 1) {
+        nextSS(e, index + 1)
+      }
     }
   }
 
+  let divRef = useRef();
+  useEffect(() => {
+    divRef.current.focus()
+  })
+
   return (
-    <div onKeyDown={onKeyDown}>
+    <div ref={divRef} onKeyDown={onKeyDown} >
       {index !== 0 ? <button
         className='leftArrow btn btn-light'
         onClick={(e) => nextSS(e, index-1)}
